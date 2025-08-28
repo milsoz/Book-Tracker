742ed9db94e8089bd6aa16c9f025c1298d111857
milosz <miloszdrew@gmail.com>
2025-08-28 19:43:49 +0200

    Styling

--- frontend/app/components/List.tsx
+++ frontend/app/components/List.tsx
@@ -52,61 +52,77 @@ function List() {
 
   return (
     <main className="w-screen h-full">
-      <div className="w-max mt-6 mx-auto grid grid-flow-row grid-cols-3 gap-6">
+      <div className="bg-zinc-800 w-full h-full">
         <form
-          className="p-2 rounded-lg col-span-3 text-zinc-50 bg-zinc-800 flex items-center justify-center"
+          className="lg:max-w-[1100px] max-w-[300px] mx-auto p-2 col-span-3 text-zinc-50 grid grid-cols-1 gap-4 lg:grid-cols-4"
           onSubmit={handleSubmit}>
-          <label htmlFor="title" className="text-lg mr-2">
-            Title
-          </label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            className="border-1 border-zinc-50 mr-10 h-5"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <label htmlFor="author" className="text-lg mr-2">
-            Author
-          </label>
-          <input
-            type="text"
-            id="author"
-            name="author"
-            className="border-1 border-zinc-50 mr-10 h-5"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-          <label htmlFor="read" className="text-lg mr-2 select-none">
-            Have read
-          </label>
-          <input
-            type="checkbox"
-            id="read"
-            name="read"
-            className="w-4 h-4 mr-4"
-            checked={read}
-            onChange={(e) => setRead(e.target.checked)}
-          />
+          <div className="flex gap-2 items-center justify-start">
+            <label htmlFor="title" className="text-lg w-max">
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="border-1 border-zinc-50 h-6 w-full"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="flex gap-2 items-center justify-start">
+            <label htmlFor="author" className="text-lg w-max">
+              Author
+            </label>
+            <input
+              type="text"
+              id="author"
+              name="author"
+              className="border-1 border-zinc-50 h-6 w-full "
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </div>
+
+          <div className="flex gap-2 items-center justify-center max-w-[160px] mx-auto">
+            <label htmlFor="read" className="text-lg select-none">
+              Have read
+            </label>
+            <input
+              type="checkbox"
+              id="read"
+              name="read"
+              className="w-4 h-4"
+              checked={read}
+              onChange={(e) => setRead(e.target.checked)}
+            />
+          </div>
           <button
             type="submit"
             className="cursor-pointer bg-green-800 w-40 rounded-lg mx-auto">
             Add book +
           </button>
         </form>
+      </div>
 
+      <div className="w-max mt-6 mx-auto grid grid-flow-row gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {books &&
           books.map((book) => (
             <div
               key={book.id}
-              className="w-[300px] h-[150px] flex flex-col bg-zinc-800 items-start justify-center text-zinc-50 p-4 rounded-2xl">
-              <h1 className="text-2xl">{book.title}</h1>
-              <h2 className="text-lg">{book.author}</h2>
+              className="w-[250px] h-auto flex flex-col bg-zinc-800 text-zinc-50 p-4 rounded-2xl">
+              <div className="min-w-0 flex-1">
+                <h1 className="text-2xl break-word leading-snug">
+                  {book.title}
+                </h1>
+                <h2 className="text-lg break-word text-zinc-300">
+                  {book.author}
+                </h2>
+              </div>
+
               {book.read === 1 ? (
                 <p className="text-green-400 mt-2">Read</p>
               ) : (
-                <div className="flex gap-2 items-center justify-center mt-2">
+                <div className="flex gap-2 items-center mt-auto">
                   <label htmlFor="markRead" className="mr-2 select-none">
                     Mark as read
                   </label>
@@ -117,7 +133,6 @@ function List() {
                     className="w-4 h-4 mr-4"
                     onChange={(e: React.ChangeEvent) => {
                       e.preventDefault();
-
                       handleChangeRead(book.id);
                     }}
                   />
