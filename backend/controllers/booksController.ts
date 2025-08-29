f0fad4fc0e666396a1f1f3290352dca1c13643a3
milosz <miloszdrew@gmail.com>
2025-08-29 15:04:38 +0200

    Promisified controller

--- backend/controllers/booksController.ts
+++ backend/controllers/booksController.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 const DB = require("../connect");
 const AppError = require("../utils/appError");
+const { promisify } = require("util");
 
 type Book = {
   id: number;
@@ -9,73 +10,73 @@ type Book = {
   read: 0 | 1;
 };
 
-exports.getSavedBooks = (req: Request, res: Response, next: NextFunction) => {
+const dbAll = promisify(DB.all).bind(DB);
+const dbGet = promisify(DB.get).bind(DB);
+const dbRun = (...args: any[]) =>
+  new Promise<void>((resolve, reject) => {
+    DB.run(...args, function (err: Error) {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+
+exports.getSavedBooks = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const sql = `SELECT * FROM books`;
 
-  DB.all(sql, [], (err: Error, rows: Book[]) => {
-    if (err) {
-      return next(
-        new AppError("Failed to get the books from our database.", 500)
-      );
-    }
-
+  try {
+    const rows: Book[] = await dbAll(sql, []);
     res.status(200).json({
       status: "success",
       books: rows,
     });
-  });
+  } catch (err) {
+    return next(
+      new AppError("Failed to get the books from our database.", 500)
+    );
+  }
 };
 
-exports.saveBook = (req: Request, res: Response, next: NextFunction) => {
+exports.saveBook = async (req: Request, res: Response, next: NextFunction) => {
   const { title, author, read } = req.body;
 
   if (!title || !author || typeof read === "undefined") {
     return next(new AppError("A book must have a title, and an author.", 400));
   }
 
-  DB.get(
-    "SELECT * FROM books WHERE title = ? AND author = ?",
-    [title, author],
-    (err: Error, row: Book) => {
-      if (err) {
-        return next(
-          new AppError(
-            "Failed to check if book is already in our database.",
-            500
-          )
-        );
-      }
+  try {
+    const existingBook: Book | undefined = await dbGet(
+      "SELECT * FROM books WHERE title = ? AND author = ?",
+      [title, author]
+    );
 
-      if (row) {
-        return next(
-          new AppError(
-            "There already is a book with this title in your list.",
-            400
-          )
-        );
-      }
-
-      const sql = `INSERT INTO books(title,author,read) VALUES (?,?,?)`;
-
-      DB.run(sql, [title, author, read], (err: Error) => {
-        if (err) {
-          return next(
-            new AppError("Failed to add the book to our database.", 500)
-          );
-        }
-
-        res.status(201).json({
-          status: "success",
-          data: {
-            book: { title, author, read },
-          },
-        });
-      });
+    if (existingBook) {
+      return next(
+        new AppError(
+          "There already is a book with this title in your list.",
+          400
+        )
+      );
     }
-  );
+
+    const sql = `INSERT INTO books(title,author,read) VALUES (?,?,?)`;
+    await dbRun(sql, [title, author, read]);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        book: { title, author, read },
+      },
+    });
+  } catch (err) {
+    return next(new AppError("Failed to add the book to our database.", 500));
+  }
 };
 
-exports.updateBookStatus = (
+exports.updateBookStatus = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -86,36 +87,22 @@ exports.updateBookStatus = (
     return next(new AppError("Please provide an id.", 400));
   }
 
-  DB.get("SELECT * FROM books WHERE id = ?", [id], (err: Error, row: Book) => {
-    if (err) {
-      return next(
-        new AppError("Failed to check if book is in our database.", 500)
-      );
-    }
-
-    if (!row) {
-      return next(
-        new AppError(
-          "There is no book with this title and author in our database",
-          500
-        )
-      );
-    }
+  try {
+    const book: Book | undefined = await dbGet(
+      "SELECT * FROM books WHERE id = ?",
+      [id]
+    );
 
     const sql = `UPDATE books SET read = ? WHERE id = ?`;
-    DB.run(sql, [1, id], (err: Error) => {
-      if (err) {
-        return next(
-          new AppError("Failed to update the book in our database.", 500)
-        );
-      }
+    dbRun(sql, [true, id]);
 
-      res.status(200).json({
-        status: "success",
-        data: {
-          book: { ...row, read: 1 },
-        },
-      });
+    res.status(200).json({
+      status: "success",
+      data: {
+        book: { ...book, read: 1 },
+      },
     });
-  });
+  } catch (err) {
+    return next(new AppError("Failed to update book status.", 500));
+  }
 };
